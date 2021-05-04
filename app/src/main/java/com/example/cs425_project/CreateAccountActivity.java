86ff3619aef4dc4c25f26fb3ce4fc87098dbd056
Andrew Weng <arzbond@gmail.com>
2021-05-04 16:11:38 -0500

    setup create Account with Firebase

--- app/src/main/java/com/example/cs425_project/CreateAccountActivity.java
+++ app/src/main/java/com/example/cs425_project/CreateAccountActivity.java
@@ -3,12 +3,50 @@ package com.example.cs425_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button createAcctButton;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    //Firestore connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private ProgressBar progressBar;
+    private EditText userNameEditText;
+    private EditText userCreditCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        createAcctButton = findViewById(R.id.creat_acct_button2);
+        progressBar = findViewById(R.id.creat_acct_progress);
+        emailEditText = findViewById(R.id.email_account);
+        passwordEditText = findViewById(R.id.password_account);
+        userNameEditText = findViewById(R.id.username_account);
+        userCreditCard = findViewById(R.id.creditcard_account);
+    }
+
+    @Override //這裡有錯誤 
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
