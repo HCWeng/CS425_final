2b69201cf89a168dae8394d0b3bf68bfea48d5a2
Andrew Weng <arzbond@gmail.com>
2021-05-04 16:51:05 -0500

    create firestore user 1

--- app/src/main/java/com/example/cs425_project/CreateAccountActivity.java
+++ app/src/main/java/com/example/cs425_project/CreateAccountActivity.java
@@ -1,15 +1,26 @@
 package com.example.cs425_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
     private Button loginButton;
@@ -40,9 +51,91 @@ public class CreateAccountActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.password_account);
         userNameEditText = findViewById(R.id.username_account);
         userCreditCard = findViewById(R.id.creditcard_account);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser != null) {
+                    //user is already loggedin
+                } else {
+                    //no user yet
+                }
+            }
+        };
+
+        createAcctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(emailEditText.getText().toString())
+                    && !TextUtils.isEmpty(passwordEditText.getText().toString())
+                    && !TextUtils.isEmpty(userNameEditText.getText().toString())
+                    && !TextUtils.isEmpty(userCreditCard.getText().toString())){
+
+                    String email = emailEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
+                    String username = userNameEditText.getText().toString().trim();
+                    String creditcard = userCreditCard.getText().toString().trim();
+                    creatUserEmailAccount(email, password, username, creditcard);
+                } else{
+                    Toast.makeText(CreateAccountActivity.this,
+                            "Empty Fields Not Allowed",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+
+
+
+            }
+        });
+
+
+    }
+    private void creatUserEmailAccount(String email, String password, String username, String creditcard){
+        if (!TextUtils.isEmpty(email)
+            && !TextUtils.isEmpty(password)
+            && !TextUtils.isEmpty(username)
+            && !TextUtils.isEmpty(creditcard)){
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                //we take user to add AddMapActivity
+                                currentUser = firebaseAuth.getCurrentUser();
+                                assert currentUser != null;
+                                String currentUserId = currentUser.getUid();
+
+                                //Create a user Map so we can create a user in the User collection
+                                Map<String, String> userObj = new HashMap<>();
+                                userObj.put("userId", currentUserId);
+                                userObj.put("username", username);
+
+                                //save to our firestore database
+
+                            }else {
+                                //something went wrong
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+        }else {
+
+        }
     }
 
-    @Override //這裡有錯誤 
+
+    @Override //這裡有錯誤
     protected void onStart() {
         super.onStart();
 
